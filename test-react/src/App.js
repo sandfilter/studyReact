03d6fed92b716c2e2a8a073a34496cd6ba04cc1f
zhou <zhou@MacBook-Pro.local>
2022-09-13 16:41:55 +0800

    add test-react

---
+++ test-react/src/App.js
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import { nanoid } from "nanoid";
+import Form from "./components/Form";
+import FilterButton from "./components/FilterButton";
+import Todo from "./components/Todo";
+
+
+function App(props) {
+
+  const [tasks, setTasks] = useState(props.tasks);
+
+function toggleTaskCompleted(id) {
+  const updatedTasks = tasks.map((task) => {
+    // if this task has the same ID as the edited task
+    if (id === task.id) {
+      // use object spread to make a new object
+      // whose `completed` prop has been inverted
+      return {...task, completed: !task.completed}
+    }
+    return task;
+  });
+  setTasks(updatedTasks);
+  //https://developer.mozilla.org/en-US/docs/Learn/Tools_and_testing/Client-side_JavaScript_frameworks/React_interactivity_events_state#completing_a_task
+}
+
+  const taskList = tasks?.map((task) => 
+    <Todo 
+    id={task.id} 
+    name={task.name} 
+    completed={task.completed} 
+    key={task.id}
+    toggleTaskCompleted={toggleTaskCompleted}
+    />
+  );
+  
+  const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
+  const headingText = `${taskList.length} ${tasksNoun} remaining`;
+
+  function addTask(name) {
+    const newTask = { id: `todo-${nanoid()}`, name, completed: false };
+    setTasks([...tasks, newTask]);
+  }
+
+  
+  
+  return (
+    <div className="todoapp stack-large">
+      <h1>TodoMatic</h1>
+      <Form addTask={addTask} />
+      <div className="filters btn-group stack-exception">
+        <FilterButton />
+        <FilterButton />
+        <FilterButton />
+      </div>
+      <h2 id="list-heading">{headingText}</h2>
+      <ul
+        role="list"
+        className="todo-list stack-large stack-exception"
+        aria-labelledby="list-heading"
+      >
+       {taskList}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
